a7998b1d8cc978267dc2544939f429c9598d5950
jdegerickx <jeroen.degerickx@vito.be>
2025-04-14 15:51:32 +0200

    resources now managed on artifactory

--- src/vito_agri_tutorials/resources/__init__.py
+++ src/vito_agri_tutorials/resources/__init__.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import geopandas as gpd
+from loguru import logger
+
+from vito_agri_tutorials.utils.artifactory import download_file
 
 
 _filenames = {
@@ -19,4 +22,13 @@ def load_resource(layername):
 
     indir = Path(__file__).resolve().parent
 
-    return gpd.read_file(indir / _filenames[layername])
+    infile = indir / _filenames[layername]
+
+    if not infile.exists():
+        logger.info(
+            f"{layername} not found in resources, downloading from Artifactory..."
+        )
+        srcpath = "resources/" + _filenames[layername]
+        download_file(indir, srcpath)
+
+    return gpd.read_file(infile)
