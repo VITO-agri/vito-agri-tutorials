1446b78e82a5eadd8661766e1a7f4387caa9d185
jdegerickx <jeroen.degerickx@vito.be>
2025-12-02 12:40:51 +0100

    new timeseries tutorials

---
+++ src/vito_agri_tutorials/BFAST/stl.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import statsmodels.tsa.seasonal as sm
+import pandas as pd
+
+
+class STL():
+    def __init__(self, y, period, periodic=True):
+        y = np.array(pd.DataFrame(y).interpolate().values.ravel().tolist())
+        # stl = sm.STL(y, period=period)
+        stl = sm.STL(y, period=period)
+        res = stl.fit()
+        seasonal = res.seasonal
+        trend = res.trend
+        residual = res.resid
+        if periodic:
+            seasonal = self.seasonal_average(seasonal, period)
+
+        self.seasonal = seasonal
+        self.trend = trend
+        self.residual = residual
+
+    def seasonal_average(self, x, period):
+        n = x.shape[0]
+        use_cut = int(n / period) * period != n
+        n_periods = int(n / period)
+        if n_periods < 1:
+            return x
+        if use_cut:
+            cut_len = period * n_periods
+            mat = x[:cut_len]
+        else:
+            mat = x
+        avg = np.mean(mat.reshape(n_periods, period), axis=0)
+        retval = np.tile(avg, n_periods + 1)
+        retval = retval[:n]
+        return retval
